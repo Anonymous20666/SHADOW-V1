e7b480075636db009e91d1f778d4144b1d1ea721
Anonymous20666 <165765773+Anonymous20666@users.noreply.github.com>
2024-10-13 05:04:09 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+//base by SHADOW 
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @AnonymousKing-pb7gg
+//Tiktok: animexanonymous1
+//Telegram: t.me/sammzie33
+//GitHub: @Anonymous2066
+//WhatsApp: +2349137924240
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@AnonymousKing-pb7gg
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
